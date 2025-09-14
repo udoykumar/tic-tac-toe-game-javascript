519a9608f0d84a536b94723c0fd9a70188fd36c9
Udoy Kumar <udoypal.kumar2002@gmail.com>
2025-09-14 19:54:14 +0600

    tic tac toe game is complete

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const boxes = document.querySelectorAll(".box");
 const resetBtn = document.querySelector("#reset-btn");
+const newGameBtn = document.getElementById("newGameBtn");
+const msgContainer = document.querySelector(".msg-continer");
+const msg = document.getElementById("msg");
+const main = document.getElementById("main");
 let turnO = true;
 
 const winPatterns = [
@@ -12,6 +16,13 @@ const winPatterns = [
   [3, 4, 5],
   [6, 7, 8],
 ];
+const resetGame = () => {
+  turnO = true;
+
+  msgContainer.classList.add("hidden");
+  main.classList.remove("hidden");
+  enableBoxes();
+};
 
 boxes.forEach((box) => {
   box.addEventListener("click", () => {
@@ -24,7 +35,48 @@ boxes.forEach((box) => {
     }
     box.disabled = true;
     checkWinner();
+    colorChange();
   });
 });
+const showWinner = (winner) => {
+  msg.innerText = `Congratulation, Winner is ${winner}`;
+  main.classList.add("hidden");
+  msgContainer.classList.remove("hidden");
+  disableBox();
+};
+const disableBox = () => {
+  for (const box of boxes) {
+    box.disabled = true;
+  }
+};
+const enableBoxes = () => {
+  for (const box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+  }
+};
 
-const checkWinner = () => {};
+const checkWinner = () => {
+  for (const pattern of winPatterns) {
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
+    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+      if (pos1Val === pos2Val && pos2Val === pos3Val) {
+        showWinner(pos1Val);
+      }
+    }
+  }
+};
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
+
+const colorChange = () => {
+  for (const box of boxes) {
+    if (box.innerText === "0") {
+      box.style.color = "green";
+    } else {
+      box.style.color = "red";
+    }
+  }
+};
